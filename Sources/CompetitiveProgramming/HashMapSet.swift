cba617c565a19f9c9d307b9f26baaf67c7155d0c
Soumen Bhuin <smbhuin@gmail.com>
2024-07-25 09:16:36 +0530

    Added solution for LeetCode 2215. Find the Difference of Two Arrays.

---
+++ Sources/CompetitiveProgramming/HashMapSet.swift
@@ -0,0 +1,28 @@
+
+public extension Solution {
+    
+    /// LeetCode: 2215. Find the Difference of Two Arrays.
+    ///
+    /// Solved using HashSet
+    ///
+    /// - Complexity: Time complexity is O(*n*), where n is the size of the array `nums`.  Space complexity is O(1), only constant space is used.
+    func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
+        let nums1 = Set(nums1)
+        let nums2 = Set(nums2)
+        var ans1: [Int] = []
+        var ans2: [Int] = []
+        for num in nums1 {
+            if !nums2.contains(num) {
+                ans1.append(num)
+            }
+        }
+        for num in nums2 {
+            if !nums1.contains(num) {
+                ans2.append(num)
+            }
+        }
+        return [ans1, ans2]
+    }
+    
+}
+
