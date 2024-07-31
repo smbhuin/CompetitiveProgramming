5858cd1fdcf268e42ecedb6190b965b3a527a6c6
Soumen Bhuin <smbhuin@gmail.com>
2024-07-31 10:04:52 +0530

    Added queue based solution for LeetCode 649. Dota2 Senate.

--- Sources/CompetitiveProgramming/Queue.swift
+++ Sources/CompetitiveProgramming/Queue.swift
@@ -20,3 +20,37 @@ class RecentCounter {
         return queue.count
     }
 }
+
+public extension Solution {
+    
+    
+    /// LeetCode: 649. Dota2 Senate.
+    ///
+    /// Solved using two queue.
+    ///
+    /// - Complexity: Time complexity is O(*n*) and space complexity is O(*n*), where *n* is the number of members in senate.
+    func predictPartyVictory(_ senate: String) -> String {
+        let senate = Array(senate)
+        var radiantQueue: [Int] = [] // Holds indices of radiants in sanate
+        var direQueue: [Int] = [] // Holds indices of dires in sanate
+        for i in 0..<senate.count {
+            if senate[i] == "R" {
+                radiantQueue.append(i);
+            }
+            else {
+                direQueue.append(i);
+            }
+        }
+        while !radiantQueue.isEmpty && !direQueue.isEmpty {
+            let r = radiantQueue.removeFirst()
+            let d = direQueue.removeFirst()
+            if r < d {
+                radiantQueue.append(r + senate.count)
+            }
+            else {
+                direQueue.append(d + senate.count)
+            }
+        }
+        return radiantQueue.isEmpty ? "Dire" : "Radiant"
+    }
+}
