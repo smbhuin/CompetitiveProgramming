988ae092c657731116e0a66d3385fae8ee726ad4
Soumen Bhuin <smbhuin@gmail.com>
2024-08-09 08:59:35 +0530

    Added solution for LeetCode 841. Keys and Rooms.

---
+++ Sources/CompetitiveProgramming/Graph.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Soumen Bhuin on 09/08/24.
+//
+
+public extension Solution {
+    
+    /// LeetCode: 841. Keys and Rooms.
+    ///
+    /// Solved using graph DFS algo.
+    ///
+    /// - Complexity: Time complexity is O(*n + e*), where *n* is the number of `rooms` and *e* is the total number of keys.
+    /// Space complexity is O(*n*), where *n* is the additional space required to store visited `rooms` and recursion stack .
+    func canVisitAllRooms(_ rooms: [[Int]]) -> Bool {
+        func dfs(_ room: Int, _ visited: inout Set<Int>) {
+            visited.insert(room)
+            let nextRooms = rooms[room]
+            for nr in nextRooms {
+                if !visited.contains(nr) {
+                    dfs(nr, &visited)
+                }
+            }
+        }
+        var visited: Set<Int> = []
+        dfs(0, &visited)
+        return visited.count == rooms.count
+    }
+    
+}
