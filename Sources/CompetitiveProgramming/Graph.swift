d60c17864776d5113d28877099d147a8afcd74e2
Soumen Bhuin <smbhuin@gmail.com>
2024-08-12 09:23:45 +0530

    Added solution for LeetCode 547. Number of Provinces.

--- Sources/CompetitiveProgramming/Graph.swift
+++ Sources/CompetitiveProgramming/Graph.swift
@@ -22,4 +22,31 @@ public extension Solution {
         return visited.count == rooms.count
     }
     
+    /// LeetCode: 547. Number of Provinces.
+    ///
+    /// Solved using graph DFS algo.
+    ///
+    /// - Complexity: Time complexity is O(*n^2*) and space complexity is O(*n*) , where *n* is the number of `cities`.
+    func findCircleNum(_ isConnected: [[Int]]) -> Int {
+        func dfs(_ city: Int, _ visited: inout Set<Int>) {
+            for (neighbor, connected) in isConnected[city].enumerated() {
+                if connected == 1 && !visited.contains(neighbor) {
+                    visited.insert(neighbor)
+                    dfs(neighbor, &visited)
+                }
+            }
+        }
+        var visited: Set<Int> = []
+        let cities = isConnected.count
+        var privinces = 0
+        for city in 0..<cities {
+            if !visited.contains(city) {
+                visited.insert(city)
+                dfs(city,&visited)
+                privinces += 1
+            }
+        }
+        return privinces
+    }
+    
 }
