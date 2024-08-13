e7d3fd18d685e5ff8f7789b8f3ef536e27c0076c
Soumen Bhuin <smbhuin@gmail.com>
2024-08-13 09:19:57 +0530

    Added test cases for LeetCode 1466. Reorder Routes to Make All Paths Lead to the City Zero.

--- Tests/CompetitiveProgrammingTests/CompetitiveProgrammingTests.swift
+++ Tests/CompetitiveProgrammingTests/CompetitiveProgrammingTests.swift
@@ -596,6 +596,17 @@ final class CompetitiveProgrammingTests: XCTestCase {
         XCTAssert(sol.findCircleNum([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]) == 1)
     }
     
+    func testMinReorder1() throws {
+        XCTAssert(sol.minReorder(6, [[0,1],[1,3],[2,3],[4,0],[4,5]]) == 3)
+    }
+    
+    func testMinReorder2() throws {
+        XCTAssert(sol.minReorder(5, [[1,0],[1,2],[3,2],[3,4]]) == 2)
+    }
+    
+    func testMinReorder3() throws {
+        XCTAssert(sol.minReorder(3, [[1,0],[2,0]]) == 0)
+    }
     
 
 }
