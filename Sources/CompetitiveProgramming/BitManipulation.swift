2de9ab655dc88426c9885b881b8974192f615d39
Soumen Bhuin <smbhuin@gmail.com>
2024-09-02 11:42:43 +0530

    Added solution for LeetCode 338. Counting Bits.

---
+++ Sources/CompetitiveProgramming/BitManipulation.swift
@@ -0,0 +1,23 @@
+
+public extension Solution {
+    
+    /// LeetCode: 338. Counting Bits.
+    ///
+    /// Solved using iteration.
+    ///
+    /// - Complexity: Time complexity is O(*nlogn*), where *n* is the provided number and space complexity is O(1), only constant space is used.
+    func countBits(_ n: Int) -> [Int] {
+        var result = Array(repeating: 0, count: n + 1)
+        for i in 0...n {
+            var num = UInt(i)
+            var count = UInt(0)
+            while num > 0 {
+                count += num & 1
+                num = num >> 1
+            }
+            result[i] = Int(count)
+        }
+        return result
+    }
+
+}
