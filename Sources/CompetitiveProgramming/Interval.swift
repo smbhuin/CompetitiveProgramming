f8b6e7c8827f50f979f00b2d2f6de7b3554e9afa
Soumen Bhuin <smbhuin@gmail.com>
2024-09-08 10:25:39 +0530

    Added solution for LeetCode 452. Minimum Number of Arrows to Burst Balloons.

--- Sources/CompetitiveProgramming/Interval.swift
+++ Sources/CompetitiveProgramming/Interval.swift
@@ -30,4 +30,26 @@ public extension Solution {
         }
         return count
     }
+    
+    /// LeetCode: 452. Minimum Number of Arrows to Burst Balloons.
+    ///
+    /// Solved using sorting & iteration.
+    ///
+    /// - Complexity: Time complexity is O(*nlogn*), where n is the size of the array `points`. Space complexity is O(1), only constant space is used.
+    func findMinArrowShots(_ points: [[Int]]) -> Int {
+        let points = points.sorted(by: { $0[0] <= $1[0] })
+        let n = points.count
+        var i = 0
+        var count = 0
+        while i < n {
+            var right = points[i][1]
+            while i + 1 < n && points[i + 1][0] <= right {
+                i += 1
+                right = min(right, points[i][1])
+            }
+            i += 1
+            count += 1
+        }
+        return count
+    }
 }
